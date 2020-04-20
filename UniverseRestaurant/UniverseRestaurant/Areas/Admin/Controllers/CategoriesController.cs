cf17ca1d66888ab84fc720ff05b23552cf29245a
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2020-04-20 15:20:19 +0300

    UniverseRestaurant 2

---
+++ UniverseRestaurant/UniverseRestaurant/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniverseRestaurant.Data;
+using UniverseRestaurant.Models;
+
+namespace UniverseRestaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoriesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await this.db.Categories.ToListAsync());
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                //if valid
+                this.db.Add(category);
+
+                await this.db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+    }
+}
