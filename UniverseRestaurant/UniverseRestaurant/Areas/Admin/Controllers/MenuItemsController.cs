aaa394ad2f0eace33a5ff53a6393781b5e94f7fc
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2020-04-20 22:34:40 +0300

    UniverseRestaurant 6 add Tinymce

--- UniverseRestaurant/UniverseRestaurant/Areas/Admin/Controllers/MenuItemsController.cs
+++ UniverseRestaurant/UniverseRestaurant/Areas/Admin/Controllers/MenuItemsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UniverseRestaurant.Data;
+using UniverseRestaurant.Models.ViewModels;
 
 namespace UniverseRestaurant.Areas.Admin.Controllers
 {
@@ -15,16 +16,30 @@ namespace UniverseRestaurant.Areas.Admin.Controllers
         private readonly ApplicationDbContext db;
         private readonly IWebHostEnvironment hostEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemsController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
         {
             this.db = db;
             this.hostEnvironment = hostEnvironment;
+            this.MenuItemVM = new MenuItemViewModel()
+            {
+                Category = this.db.Categories,
+                MenuItem = new Models.MenuItem()
+            };
         }
         public async Task<IActionResult> Index()
         {
             var menuItems = await this.db.MenuItems.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
 
             return View(menuItems);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return this.View(MenuItemVM);
         }
     }
 }
