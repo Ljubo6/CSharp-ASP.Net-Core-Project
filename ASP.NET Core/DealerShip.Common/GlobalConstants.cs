72c292b88b9f5ea497985d91f17049af30389d4d
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2020-04-20 12:35:37 +0300

    Deleting

--- ASP.NET Core/DealerShip.Common/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DealerShip.Common
-{
-    public static class GlobalConstants
-    {
-        public const string SystemName = "DealerShip";
-
-        public const string AdministratorRoleName = "Administrator";
-    }
-}
--- ASP.NET Core/Services/DealerShip.Services.Mapping/IHaveCustomMappings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DealerShip.Services.Mapping
-{
-    using AutoMapper;
-
-    public interface IHaveCustomMappings
-    {
-        void CreateMappings(IProfileExpression configuration);
-    }
-}
