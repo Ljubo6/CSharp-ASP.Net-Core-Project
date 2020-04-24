7042764acda9eb2b0eeed1fa0e9b468a89d19aa1
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2020-04-24 04:07:10 +0300

    UniverseRestaurant 16 bug in Coupon

---
+++ UniverseRestaurant/UniverseRestaurant/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniverseRestaurant.Data;
+using UniverseRestaurant.Models.ViewModels;
+using UniverseRestaurant.Utility;
+
+namespace UniverseRestaurant.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        [BindProperty]
+        public OrderDetailsCart detailCart { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            detailCart = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader(),
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0m;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = this.db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+
+            if (cart != null)
+            {
+                detailCart.listCart = cart.ToList();
+            }
+
+            foreach (var list in detailCart.listCart)
+            {
+                list.MenuItem = await this.db.MenuItems.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+
+                detailCart.OrderHeader.OrderTotal = detailCart.OrderHeader.OrderTotal + ((decimal)list.MenuItem.Price * list.Count);
+                list.MenuItem.Description = StaticDetail.ConvertToRawHtml(list.MenuItem.Description);
+
+                if (list.MenuItem.Description.Length > 100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0,99) + "...";
+                }               
+            }
+            detailCart.OrderHeader.OrderTotalOriginal = detailCart.OrderHeader.OrderTotal;
+
+            if (HttpContext.Session.GetString(StaticDetail.ssCouponCode) != null)
+            {
+                detailCart.OrderHeader.CouponCode = HttpContext.Session.GetString(StaticDetail.ssCouponCode);
+                var couponFromDb = await this.db.Coupons.Where(c => c.Name.ToLower() == detailCart.OrderHeader.CouponCode.ToLower()).FirstOrDefaultAsync();
+
+                detailCart.OrderHeader.OrderTotal = (decimal)StaticDetail.DiscountedPrice(couponFromDb,detailCart.OrderHeader.OrderTotalOriginal);
+            }
+
+            return View(detailCart);
+        }
+        public IActionResult AddCoupon()
+        {
+            if (detailCart.OrderHeader.CouponCode == null)
+            {
+                detailCart.OrderHeader.CouponCode = "";
+            }
+
+            HttpContext.Session.SetString(StaticDetail.ssCouponCode,detailCart.OrderHeader.CouponCode);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- UniverseRestaurant/UniverseRestaurant/Utility/StaticDetail.cs
+++ UniverseRestaurant/UniverseRestaurant/Utility/StaticDetail.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UniverseRestaurant.Models;
 
 namespace UniverseRestaurant.Utility
 {
@@ -15,7 +16,64 @@ namespace UniverseRestaurant.Utility
         public const string CustomerEndUser = "Customer";
 
         public const string ssShoppingCartCount = "ssCartCount";
+        public const string ssCouponCode = "ssCouponCode";
 
+        public static string ConvertToRawHtml(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
 
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
+
+        public static decimal DiscountedPrice(Coupon couponFromDb, decimal OriginalOrderTotal)
+        {
+            if (couponFromDb == null)
+            {
+                return OriginalOrderTotal;
+            }
+            else
+            {
+                if ((decimal)couponFromDb.MinimumAmount > OriginalOrderTotal)
+                {
+                    return OriginalOrderTotal;
+                }
+                else
+                {
+                    //everything is valid
+                    if (Convert.ToInt32(couponFromDb.CouponType) == (int)Coupon.ECouponType.Dollar)
+                    {
+                        //$10 off $100
+                        return Math.Round(OriginalOrderTotal - (decimal)couponFromDb.Discount, 2);
+                    }
+                    if (Convert.ToInt32(couponFromDb.CouponType) == (int)Coupon.ECouponType.Percent)
+                    {
+                        //10% off $100
+                        return Math.Round(OriginalOrderTotal - (OriginalOrderTotal * (decimal)couponFromDb.Discount / 100), 2);
+                    }
+                }
+            }
+            return OriginalOrderTotal;
+        }
     }
 }
